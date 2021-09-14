fe9b416a28e31c56d3f92afaad2b1add58537208
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2021-09-14 17:23:59 -0400

    Add intro message

---
+++ src/App/Intro/index.js
@@ -0,0 +1,14 @@
+import './index.css';
+
+function Intro() {
+  return (
+    <div>
+      <h1 className="display-1">
+        We are Erstel
+      </h1>
+      <h3 className="text-center mt-3" style={{letterSpacing: '0.4em'}} >We make games</h3>
+    </div>
+  );
+}
+
+export default Intro;
---
+++ src/App/index.js
@@ -0,0 +1,12 @@
+import Intro from './Intro';
+import './index.css';
+
+function App() {
+  return (
+    <div className="d-flex h-100 align-items-center justify-content-center">
+      <Intro/>
+    </div>
+  );
+}
+
+export default App;
