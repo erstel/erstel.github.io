fd10857772b9c290604add75fc5285367fd4b251
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2021-09-14 21:58:44 -0400

    Update logo placeholder in navbar

--- src/App/Navbar/index.js
+++ src/App/Navbar/index.js
@@ -4,7 +4,9 @@ import './index.css'
 function Navbar() {
   return (
     <nav className="navbar position-fixed w-100 p-0">
-      <a href="/" className="h-100 px-5">E</a>
+      <NavLink exact to="/" className="h-100 px-5">
+        logo
+      </NavLink>
       <NavLink className="-page-link navbar-item h-100 px-5" to="/games" exact>
         Games
       </NavLink>
