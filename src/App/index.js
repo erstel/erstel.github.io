a62d29798eeb93f868439bcb574b219d07ac79aa
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2021-09-14 22:17:52 -0400

    Use HashRouter instead of BrowserRouter

--- src/App/index.js
+++ src/App/index.js
@@ -1,18 +1,18 @@
-import { Route, BrowserRouter } from 'react-router-dom';
+import { Route, HashRouter } from 'react-router-dom';
 import Navbar from './Navbar';
 import Home from './Home'
 import './index.css';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Navbar />
       <div className="d-flex h-100 align-items-center justify-content-center">
         <Route exact path="/">
           <Home />
         </Route>
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
