add7271a41765590f308c4362a04a78c7fae52ac
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2021-09-15 17:01:32 -0400

    Remove tagline on home page

--- src/App/Home/Intro/index.js
+++ src/App/Home/Intro/index.js
@@ -6,7 +6,6 @@ function Intro() {
       <h1 className="display-1">
         We are Erstel
       </h1>
-      <h3 className="text-center mt-3" style={{letterSpacing: '0.4em'}} >We make games</h3>
     </div>
   );
 }
