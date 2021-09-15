9a4c64d0033bc54708ca45ab12285b9acf947803
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2021-09-14 21:36:43 -0400

    Add /games

---
+++ src/App/Games/index.js
@@ -0,0 +1,32 @@
+import { NavLink } from "react-router-dom";
+import "./index.css";
+
+function Games() {
+  /* 
+    return a nested grid.  The first grid distributes screen space to each game,
+    with each cell expanding its height to fill the screen. The inner grid
+    vertically aligns the h2 within each of the outer cells.
+   */
+  return (
+    <div className="container h-100">
+      <div className="row h-100">
+        <NavLink exact to="/games/protosim" className="-game-link col h-100">
+          <div className="row h-100 align-items-center">
+            <h2 className="col text-center">
+              Protosim
+            </h2>
+          </div>
+        </NavLink>
+        <NavLink exact to="/games/phone-call-home" className="-game-link col">
+          <div className="row h-100 align-items-center">
+            <h2 className="col text-center">
+              Phone Call Home
+            </h2>
+          </div>
+        </NavLink>
+      </div>
+    </div>
+  )
+}
+
+export default Games;
--- src/App/index.js
+++ src/App/index.js
@@ -1,6 +1,7 @@
 import { Route, HashRouter } from 'react-router-dom';
 import Navbar from './Navbar';
 import Home from './Home'
+import Games from './Games';
 import './index.css';
 
 function App() {
@@ -10,6 +11,10 @@ function App() {
       <div className="d-flex h-100 align-items-center justify-content-center">
         <Route exact path="/">
           <Home />
+        </Route>
+
+        <Route exact path="/games">
+          <Games />
         </Route>
       </div>
     </HashRouter>
