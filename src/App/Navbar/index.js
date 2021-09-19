c44bf00e32d66566618ee997f84f815c124618cf
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2021-09-19 14:02:08 -0400

    Add spacing equal to the navbar's height

--- src/App/Navbar/index.js
+++ src/App/Navbar/index.js
@@ -3,19 +3,23 @@ import './index.css'
 
 function Navbar() {
   return (
-    <nav className="navbar position-fixed w-100 p-0">
-      <NavLink exact to="/" className="h-100 px-5">
-        logo
-      </NavLink>
-      <div className="h-100">
-        <NavLink className="-page-link navbar-item d-inline-block h-100 px-5" to="/games" exact>
-          Games
+    <div>
+      <nav className="navbar position-fixed w-100 p-0">
+        <NavLink exact to="/" className="h-100 px-5">
+          logo
         </NavLink>
-        <NavLink className="-page-link navbar-item d-inline-block h-100 px-5" to="/team" exact>
-          Team
-        </NavLink>
-      </div>
-    </nav>
+        <div className="h-100">
+          <NavLink className="-page-link navbar-item d-inline-block h-100 px-5" to="/games" exact>
+            Games
+          </NavLink>
+          <NavLink className="-page-link navbar-item d-inline-block h-100 px-5" to="/team" exact>
+            Team
+          </NavLink>
+        </div>
+      </nav>
+      { /* Add space above content that matches the navbar height */ }
+      <div className="-navbar-spacer"></div>
+    </div>
   )
 }
 
