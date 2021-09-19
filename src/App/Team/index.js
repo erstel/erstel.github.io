2fa1609c205a929504cf8024624ea92a0f2cbe4b
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2021-09-19 14:06:36 -0400

    Populate /team

--- src/App/Team/index.js
+++ src/App/Team/index.js
@@ -1,15 +1,47 @@
+import { useState } from 'react'
+
 import "./index.css";
 
 function Team() {
+  const [blurred, setBlurred] = useState(false);
+
   return (
-    <div className="-team w-100">
-      <h1 className="display-4">Meet the team</h1>
-      <div className="row mt-4">
-        <div className="col">
-          Daniel Kelly
+    <div className="-team w-100 mt-5">
+      <h1 className="display-4 mb-5">Meet the team</h1>
+      <div className="-team-top-line row m-0"></div>
+      <div className="row w-100 m-0">
+        <div
+          className="col w-100 m-0 p-0"
+          onMouseEnter={() => setBlurred(true)}
+          onMouseLeave={() => setBlurred(false)}
+          >
+          <img
+            src="/images/daniel.jpg"
+            alt="Daniel Kelly"
+            className={"-team-image" + (blurred ? " -team-image-blurred" : "")}
+            onMouseEnter={() => setBlurred(true)}
+            onMouseLeave={() => setBlurred(false)}
+            />
+          <h2 className="display-5 text-center mt-0">Daniel Kelly</h2>
+          <div className="-team-member-info text-center">
+            <p>Bio here</p>
+          </div>
         </div>
-        <div className="col">
-          Caleb Sacks
+        <div
+          className="col m-0 p-0"
+          onMouseEnter={() => setBlurred(true)}
+          onMouseLeave={() => setBlurred(false)}
+          >
+          <img
+            src="/images/caleb.jpg"
+            alt="Caleb Sacks"
+            className={"-team-image" + (blurred ? " -team-image-blurred" : "")}
+            />
+          <h2 className="display-5 text-center mt-0">Caleb Sacks</h2>
+          <div className="-team-member-info text-center">
+            Co-owner, co-designer, developer and composer. When I'm not working
+            on games, I like to hang out with friends and play Minecraft.
+          </div>
         </div>
       </div>
     </div>
