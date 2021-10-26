2d68b02257722ceac539919bade193b90e56ea90
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2021-10-26 13:42:53 -0400

    Fix social icons not positioned at the bottom
    The footer now positions its elements using a grid.

--- src/App/Footer/index.js
+++ src/App/Footer/index.js
@@ -3,23 +3,25 @@ import "./index.css";
 
 function Footer() {
   return (
-    <div className="-footer">
-      <div className="text-muted">Copyright &copy; 2021 Erstel</div>
+    <div className="-footer container-fluid">
+      <div className="row">
+        <div className="col"></div>
+        <div className="-footer-copyright col text-muted">Copyright &copy; 2021 Erstel</div>
 
-      <div className="-footer-social-media-links">
-        <a href="https://discord.gg/2bvBxkG" target="_blank" rel="noopener noreferrer">
-          <img src="/images/social/discord.png" alt="Discord" className="mr-3" />
-        </a>
+        <div className="-footer-social-media-links col">
+          <a href="https://discord.gg/2bvBxkG" target="_blank" rel="noopener noreferrer">
+            <img src="/images/social/discord.png" alt="Discord" className="mr-3" />
+          </a>
 
-        <a href="https://erstel.itch.io/" target="_blank" rel="noopener noreferrer">
-          <img src="/images/social/itch.png" alt="Itch" className="mr-3" />
-        </a>
+          <a href="https://erstel.itch.io/" target="_blank" rel="noopener noreferrer">
+            <img src="/images/social/itch.png" alt="Itch" className="mr-3" />
+          </a>
 
-        <a href="https://twitter.com/erstelgames" target="_blank" rel="noopener noreferrer">
-          <img src="/images/social/twitter.png" alt="Twitter" className="mr-3" />
-        </a>
+          <a href="https://twitter.com/erstelgames" target="_blank" rel="noopener noreferrer">
+            <img src="/images/social/twitter.png" alt="Twitter" className="mr-3" />
+          </a>
+        </div>
       </div>
-
     </div>
   );
 }
