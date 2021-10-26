954ab1ec6ad778da2427a2cbd1bcea6bdb6ba100
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2021-10-26 17:20:27 -0400

    Update protosim's description

--- src/App/Games/protosim/index.js
+++ src/App/Games/protosim/index.js
@@ -14,8 +14,25 @@ function Protosim() {
         </h1>
 
         <p>
-          Rescue a group of testers from a simulation gone wrong...
+          Your task is to rescue a group of humans from a military testing
+          simulation. You're rescuing them from the
+          <span className="-important"> Fears</span>.
         </p>
+
+        <p>
+          The Fears were designed for the military, but after gaining
+          intelligence and self-awareness, they turned against us. Now, the only
+          way we can rescue the humans without risking their death in real life
+          is to lead them through a portal.
+        </p>
+
+        <p>
+          In the sim you can <span className="-important">reverse time </span>
+          and place <span className="-important">force fields</span> that freeze
+          nearby Fears so you can lead each tester to safety.
+        </p>
+
+        <hr />
 
         <div>
           <em>
